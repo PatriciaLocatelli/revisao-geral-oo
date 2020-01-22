32a0e3151394544c412bb0dfeb68eeec8544b0c3
patriciaLocatelli <paty.lw502@gmail.com>
2020-01-22 11:58:47 -0300

    Adicionado o construtor

---
+++ src/entidades/Produto.java
@@ -0,0 +1,31 @@
+package entidades;
+
+public class Produto {
+    public String nome;
+    public double preco;
+    public int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public int totalEmEstoque() {
+        return this.quantidade;
+    }
+
+    public void adicionarProdutos(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void deletarProdutos(int quantidade){
+        this.quantidade -= quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome = " + nome + " Preço = " + preco +
+                " Quantidade = " + quantidade;
+    }
+}
